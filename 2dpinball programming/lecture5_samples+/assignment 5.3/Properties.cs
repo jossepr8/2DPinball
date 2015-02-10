de93004f5125cfdda33fc712a9f56ff978cc0f60
Kougra123 <timv@aol.nl>
2015-02-10 13:54:56 +0500

    Properties reader

--- 2dpinball programming/lecture5_samples+/assignment 5.3/Properties.cs
+++ 2dpinball programming/lecture5_samples+/assignment 5.3/Properties.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Xml;
+using GXPEngine;
+using System.Drawing;
 
 namespace GXPEngine
 {
@@ -8,7 +11,34 @@ namespace GXPEngine
 
 		public Properties ()
 		{
+            
 		}
+
+        public void Read()
+        {
+            XmlReader reader = XmlReader.Create("properties.xml");
+            ReadBall(reader);
+        }
+
+        void ReadBall(XmlReader reader)
+        {
+            reader.ReadStartElement("Properties");
+            reader.ReadStartElement("Ball");
+            reader.ReadStartElement("Gravity");
+            int g = reader.ReadContentAsInt();
+            Gravity = new Vec2(0f, g);
+            reader.ReadEndElement();
+            reader.ReadEndElement();
+            reader.ReadEndElement();
+        }
+        
+
+       
+
+
+        
+
+
 	}
 }
 
