86f2b17c7a133d14e2fc6145a7bc1ed729c72edd
Kougra123 <timv@aol.nl>
2015-02-18 11:32:26 +0100

    animated enemies

--- 2dpinball programming/lecture5_samples+/assignment 5.3/Enemy.cs
+++ 2dpinball programming/lecture5_samples+/assignment 5.3/Enemy.cs
@@ -2,23 +2,43 @@
 
 namespace GXPEngine
 {
-	public class Enemy : Sprite
+	public class Enemy : AnimSprite
 	{
 		public float speed {
 			get;
 			set;
 		}
 
+		private float frame = 0;
+		private float minframe = 0;
+		private float maxframe = 8;
 
-		public Enemy () : base("Enemy.png")
+		public Enemy () : base("sheet.png",8,1)
 		{	
 			SetScaleXY (0.5f, 0.5f);
 			SetOrigin (width / 2, height / 2);
 			speed = Properties.EnemyGravity;
 		}
-			
 
-		public void Step(){
+		private void SetFrames(int min, int max){
+			minframe = min;
+			maxframe = max;
+		}
+
+		public void Step()
+		{	
+			frame += 0.12f;
+
+			SetFrames (1, 8);
+
+			if (frame >= maxframe + 1) {
+				frame = minframe;
+			}
+			if (frame < minframe) {
+				frame = maxframe;
+			}
+			SetFrame ((int)frame);
+
 			y += speed;
 		}
 
